229e63d419d4a2fb83cf86924ce08e9c0d17b838
MiWeed2006 <michaelweedjr@gmail.com>
2026-01-14 12:31:50 -0500

    Initial commit

---
+++ src/SchoolProgram.java
@@ -0,0 +1,18 @@
+public class SchoolProgram {
+    static void main() {
+        String[] itemNames = {"Granola", "Trail Mix", "Pizza",
+                              "Water", "Gatorade", "Fruit Punch",
+                              "Mechanical Pencils", "Pencils", "Pens"
+                             };
+        String[][] categories = {{"Snacks", "Granola", "Trail Mix", "Pizza"},
+                                 {"Beverages", "Water", "Gatorade", "Fruit Punch"},
+                                 {"Writing Untencils", "Mechanical Pencils", "Pencils", "Pens"}
+                                 };
+        double[] itemPrices = {9.99, 8.99, 7.99,
+                               1.00, 2.00, 2.50,
+                               1.50, 1.00, 3.00
+                               };
+
+
+    }
+}
